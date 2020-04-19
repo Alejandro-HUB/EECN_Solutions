ad2e60ef0908592f5c402a5fca1f361c074f0b84
Alejandro <alejandro-lopez-martinez@hotmail.com>
2020-04-18 22:26:01 -0400

    Initial commit

---
+++ EECN_Solutions/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EECN_Solutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
