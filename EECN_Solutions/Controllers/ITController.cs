09764369885f80d8661809aedb3ef09d1e66c77b
Mauricio López <mauricio_mla@hotmail.com>
2020-04-21 02:39:05 -0400

    New version

---
+++ EECN_Solutions/Controllers/ITController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EECN_Solutions.Controllers
+{
+    public class ITController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
